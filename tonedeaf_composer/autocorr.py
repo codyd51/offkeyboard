8bef794f120b1f35eabc98adc6a161744eeb8148
Phillip Tennen <pt@datatheorem.com>
2019-04-26 00:01:44 +0200

    bandpass audio frame to guitar frequencies

--- tonedeaf_composer/autocorr.py
+++ tonedeaf_composer/autocorr.py
@@ -1,22 +1,27 @@
-import pyaudio
+import time
+import collections
+
 import numpy as np
 from numpy import argmax, diff
 from matplotlib.mlab import find
-from scipy.signal import fftconvolve
+from scipy.signal import butter, lfilter, fftconvolve
+
 from tonedeaf_composer.parabolic import parabolic
+from tonedeaf_composer.frame_provider import WavFileFrameProvider, MicrophoneFrameProvider
 
 
 NOTE_MIN = 40        # E2
 NOTE_MAX = 64        # E4
-SAMPLE_RATE = 22050  # sampling frequency in Hz
-FRAME_SIZE = 2048    # samples per frame
-FRAMES_PER_FFT = 16  # run FFT over how many frames?
+
+SAMPLE_RATE = 22050         # sampling frequency in Hz
+FRAMES_PER_FFT = 16         # run FFT over how many frames?
+SAMPLES_PER_FRAME = 2048    # samples per frame
 
 # Derived quantities from constants above. Note that as
 # SAMPLES_PER_FFT goes up, the frequency step size decreases (sof
 # resolution increases); however, it will incur more delay to process
 # new sounds.
-SAMPLES_PER_FFT = FRAME_SIZE * FRAMES_PER_FFT
+SAMPLES_PER_FFT = SAMPLES_PER_FRAME * FRAMES_PER_FFT
 FREQ_STEP = float(SAMPLE_RATE) / SAMPLES_PER_FFT
 
 # Note/frequency conversion functions come from:
@@ -71,11 +76,24 @@ def plot(data: np.array, title='') -> None:
     pylab.show()
     pass
 
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+
 print('sampling at', SAMPLE_RATE, 'Hz with max resolution of', FREQ_STEP, 'Hz')
 print()
 
-import collections
-import time
 d = collections.deque(maxlen=10)
 
 
@@ -141,7 +159,17 @@ else:
 
 
 num_frames = 0
+
+guitar_min_freq = max(0, int(np.floor(note_to_fftbin(NOTE_MIN-1))))
+guitar_max_freq = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(NOTE_MAX+1))))
+
 while frame_provider.has_frames():
+    audio_frame = frame_provider.get_frame()
+    # plot(raw_frame, title='before bandpass')
+    # band-pass the frame to remove data outside guitar frequencies
+    audio_frame = butter_bandpass_filter(audio_frame, guitar_min_freq, guitar_max_freq, SAMPLE_RATE)
+    # plot(raw_frame, title='after bandpass')
+
     # Shift the buffer down and new data in
     buf[:-SAMPLES_PER_FRAME] = buf[SAMPLES_PER_FRAME:]
     buf[-SAMPLES_PER_FRAME:] = audio_frame
@@ -159,6 +187,8 @@ while frame_provider.has_frames():
     # print('{:>3s} {:+.2f} ({:7.2f}) @ {:7.2f} Hz'.format(note_name(n0), n - n0, n, freq))
 
     note = note_name(n0)
-    reader.process_note(note)
+    print(note)
+    # reader.process_note(note)
+    # plot(buf)
 
     continue
