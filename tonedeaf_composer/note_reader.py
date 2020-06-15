d2eab7e36f6717a47d2a0df50b17e6fd09967c88
Phillip Tennen <pt@datatheorem.com>
2020-06-15 19:15:54 +0200

    flesh out mouse support

--- tonedeaf_composer/note_reader.py
+++ tonedeaf_composer/note_reader.py
@@ -1,10 +1,19 @@
 import collections
-from typing import Optional
+from typing import Optional, Union, List
 
 import keyboard
 
-from keymaps import MinecraftMap
+from keymaps import MarioMap
 from note_utils import SILENCE_NOTE
+from mouse import (
+    MOUSE_KEYS,
+    MOUSE_LEFT_KEY,
+    MOUSE_RIGHT_KEY,
+    MOUSE_UP_KEY,
+    MOUSE_DOWN_KEY,
+    MOUSE_CLICK_KEY,
+    VirtualMouse
+)
 
 
 class NoteReader:
@@ -12,16 +21,20 @@ class NoteReader:
         self.ringbuf_size = 10
         self.last_notes = collections.deque(maxlen=self.ringbuf_size)
         self.last_pressed_note = None
-        self.keymap = MinecraftMap()
+        self.keymap = MarioMap()
         self.currently_held_key: Optional[str] = None
 
     def release_held_key(self):
         """Release the currently held key
         """
         if not self.currently_held_key:
+
             return
         print(f'Releasing key: {self.currently_held_key}')
-        keyboard.release(self.currently_held_key)
+        if self.currently_held_key not in MOUSE_KEYS:
+            keyboard.release(self.currently_held_key)
+        else:
+            VirtualMouse.clear()
         self.currently_held_key = None
 
     def hold_key(self, note: str, key: str) -> None:
@@ -41,6 +54,10 @@ class NoteReader:
 
         # clean up the key we were just pressing
         self.release_held_key()
+
+        if note == SILENCE_NOTE and self.currently_held_key in MOUSE_KEYS:
+            VirtualMouse.clear()
+            return
 
         # If this is a key that should be pressed until the note changes, do so
         if self.keymap.should_hold_key(key):
@@ -63,6 +80,25 @@ class NoteReader:
 
         key = self.keymap.key_for_note(note)
         if not key:
+        # Mouse keys are handled separately
+        if key_or_key_list in MOUSE_KEYS:
+            key = key_or_key_list
+            print(f'Ignoring mouse event: {key}')
+            return
+
+            print(f'got a mouse event: {key}')
+            self.currently_held_key = key
+            if key == MOUSE_LEFT_KEY:
+                VirtualMouse.hold_left()
+            elif key == MOUSE_RIGHT_KEY:
+                VirtualMouse.hold_right()
+            elif key == MOUSE_UP_KEY:
+                VirtualMouse.hold_up()
+            elif key == MOUSE_DOWN_KEY:
+                VirtualMouse.hold_down()
+            elif key == MOUSE_CLICK_KEY:
+                self.currently_held_key = None
+                VirtualMouse.left_click()
             return
 
         print(f'{note}\tPressing "{key}"')
