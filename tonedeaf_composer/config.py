c8c77bb1a292e4941cb61dcacc6f8af5fc14b194
Phillip Tennen <pt@datatheorem.com>
2020-06-15 19:16:10 +0200

    add keymap for Mario

--- tonedeaf_composer/config.py
+++ tonedeaf_composer/config.py
@@ -2,30 +2,35 @@
 # Settings you should feel free to play around with.
 # #####
 
-JUMP_NOTE = 'E2'
+JUMP_NOTE = 'F#3'
 
 STRAFE_LEFT_NOTE = 'F2'
 STRAFE_DOWN_NOTE = 'F#2'
 STRAFE_UP_NOTE = 'G2'
 STRAFE_RIGHT_NOTE = 'G#2'
 
-MOUSE_LEFT_NOTE = 'D#3'
-MOUSE_DOWN_NOTE = 'E3'
-MOUSE_UP_NOTE = 'F3'
-
-MOUSE_RIGHT_NOTE = 'F#3 '
-
-CRAFT_NOTE = 'A3'
-ATTACK_NOTE = 'A#3'
-PICK_BLOCK_NOTE = 'B3'
-PLACE_BLOCK_NOTE = 'C3'
+ONE_NOTE = 'G#3'
+TWO_NOTE = 'A4'
+THREE_NOTE = 'A#4'
+FOUR_NOTE = 'B4'
+FIVE_NOTE = 'C4'
+SIX_NOTE = 'C#4'
+SEVEN_NOTE = 'D4'
+EIGHT_NOTE = 'D#4'
+NINE_NOTE = 'E4'
+ZERO_NOTE = 'F4'
 
 ESC_NOTE = 'D3'
+
+COMBO_KEYS_TO_NOTES = {
+    'F3': ['up', 'left'],
+    'F#4': ['up', 'right'],
+}
 
 MIN_VOLUME = 100000  # Any audio below this value is considered silence. Chosen through experimentation
 
 # #####
-# You probably won't need to modify things below this line, as they deal with signal processing
+# You probably won't need to modify things in this section, as they deal with signal processing
 # #####
 
 NOTE_MIN = 40        # E2
@@ -35,3 +40,21 @@ NOTE_MAX = 64        # E4
 SAMPLE_RATE = 22050         # sampling frequency in Hz
 FRAMES_PER_FFT = 16         # run FFT over how many frames?
 SAMPLES_PER_FRAME = 1024    # samples per frame
+
+
+# #####
+# For Minecraft
+# #####
+
+MOUSE_LEFT_NOTE = 'D#3'
+MOUSE_DOWN_NOTE = 'E3'
+MOUSE_UP_NOTE = 'F3'
+MOUSE_RIGHT_NOTE = 'F#3'
+MOUSE_CLICK_NOTE = 'G3'
+MOUSE_SPEED = 30
+
+CRAFT_NOTE = 'A3'
+ATTACK_NOTE = 'A#3'
+PICK_BLOCK_NOTE = 'B3'
+PLACE_BLOCK_NOTE = 'C3'
+
--- tonedeaf_composer/keymaps.py
+++ tonedeaf_composer/keymaps.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from config import (
     ESC_NOTE,
@@ -9,10 +9,30 @@ from config import (
     STRAFE_LEFT_NOTE,
     STRAFE_DOWN_NOTE,
     STRAFE_RIGHT_NOTE,
+    COMBO_KEYS_TO_NOTES,
 
     PICK_BLOCK_NOTE,
-    PLACE_BLOCK_NOTE
+    PLACE_BLOCK_NOTE,
+
+    MOUSE_UP_NOTE,
+    MOUSE_LEFT_NOTE,
+    MOUSE_DOWN_NOTE,
+    MOUSE_RIGHT_NOTE,
+    MOUSE_CLICK_NOTE,
+
+    ONE_NOTE,
+    TWO_NOTE,
+    THREE_NOTE,
+    FOUR_NOTE,
+    FIVE_NOTE,
+    SIX_NOTE,
+    SEVEN_NOTE,
+    EIGHT_NOTE,
+    NINE_NOTE,
+    ZERO_NOTE
 )
+
+from mouse import MOUSE_UP_KEY, MOUSE_DOWN_KEY, MOUSE_LEFT_KEY, MOUSE_RIGHT_KEY, MOUSE_CLICK_KEY
 
 
 class Keymap:
@@ -27,10 +47,12 @@ class Keymap:
             return None
         return self.keymap[note]
 
-    def should_hold_key(self, key: str) -> bool:
+    def should_hold_key(self, key_or_key_list: Union[str, List]) -> bool:
         """When this key is registered, should it be quickly pressed and released, or held until explicitly released?
         """
-        return key in self.held_keys
+        if isinstance(key_or_key_list, List):
+            return any(x in self.held_keys for x in key_or_key_list)
+        return key_or_key_list in self.held_keys
 
 
 class Keymap1(Keymap):
@@ -84,24 +106,76 @@ class Keymap2(Keymap):
 class MinecraftMap(Keymap):
     def __init__(self):
         # When playing Minecraft, we want to hold down the movement keys instead of quickly pressing them.
-        held_keys = [' ', 'left', 'down', 'up', 'right']
+        held_keys = [' ', 'left', 'down', 'up', 'right', MinecraftMap.key_for_note(ATTACK_NOTE)]
         super().__init__([], [], held_keys=held_keys)
 
-    def key_for_note(self, note: str) -> Optional[str]:
+    @classmethod
+    def key_for_note(cls, note: str) -> Optional[str]:
         config_keys = {
             ESC_NOTE: 'esc',
             JUMP_NOTE: ' ',
             CRAFT_NOTE: 'e',
             ATTACK_NOTE: 'p',
 
-            PICK_BLOCK_NOTE: 'o',
+            PICK_BLOCK_NOTE: 'z',
             PLACE_BLOCK_NOTE: 'i',
 
             STRAFE_LEFT_NOTE: 'left',
             STRAFE_RIGHT_NOTE: 'right',
             STRAFE_UP_NOTE: 'up',
             STRAFE_DOWN_NOTE: 'down',
+
+            # MOUSE_LEFT_NOTE: MOUSE_LEFT_KEY,
+            # MOUSE_RIGHT_NOTE: MOUSE_RIGHT_KEY,
+            # MOUSE_UP_NOTE: MOUSE_UP_KEY,
+            # MOUSE_DOWN_NOTE: MOUSE_DOWN_KEY,
+            # MOUSE_CLICK_NOTE: MOUSE_CLICK_KEY,
+
+            ONE_NOTE: 'a',
+            TWO_NOTE: '2',
+            THREE_NOTE: '3',
+            FOUR_NOTE: '4',
+            FIVE_NOTE: '5',
+            SIX_NOTE: '6',
+            SEVEN_NOTE: '7',
+            EIGHT_NOTE: '8',
+            NINE_NOTE: '9',
+            ZERO_NOTE: '0'
         }
         if note in config_keys:
             return config_keys[note]
+        return None
+
+
+class MarioMap(Keymap):
+    def __init__(self):
+        # When playing Mario, we want to hold down the movement keys instead of quickly pressing them.
+        held_keys = [' ', 'left', 'down', 'up', 'right', MinecraftMap.key_for_note(ATTACK_NOTE)]
+        super().__init__([], [], held_keys=held_keys)
+
+    @classmethod
+    def key_for_note(cls, note: str) -> Optional[Union[str, List]]:
+        config_keys = {
+            ESC_NOTE: 'esc',
+
+            STRAFE_LEFT_NOTE: 'left',
+            STRAFE_RIGHT_NOTE: 'right',
+            STRAFE_UP_NOTE: 'up',
+            STRAFE_DOWN_NOTE: 'down',
+
+            ONE_NOTE: 'a',
+            TWO_NOTE: '2',
+            THREE_NOTE: '3',
+            FOUR_NOTE: '4',
+            FIVE_NOTE: '5',
+            SIX_NOTE: '6',
+            SEVEN_NOTE: '7',
+            EIGHT_NOTE: '8',
+            NINE_NOTE: '9',
+            ZERO_NOTE: '0'
+        }
+        if note in config_keys:
+            return config_keys[note]
+        if note in COMBO_KEYS_TO_NOTES:
+            return COMBO_KEYS_TO_NOTES[note]
         return None
